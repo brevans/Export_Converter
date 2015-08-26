8b373d97549017435688b1f284a98e83fc9caff6
Ben Evans <benjamin.robert.evans@gmail.com>
2015-08-26 11:55:12 -0400

    changed to accept fam files to filter output
    use fam files as input to allow for coding sex and penotpye before
    output

--- export2plink.py
+++ export2plink.py
@@ -24,7 +24,7 @@ def translate(annot, gt_row):
     return t_gts
 
 
-def make_ped_file(snp_list, annot_file, pop_file, gt_file, out_pre):
+def make_ped_file(snp_list, annot_file, fam_file, gt_file, out_pre):
     """
     The PED file is a white-space (space or tab) delimited file.
     the first six columns are mandatory:
@@ -56,10 +56,10 @@ def make_ped_file(snp_list, annot_file, pop_file, gt_file, out_pre):
             kept_annots[0] = kept_annots[0].split('.')[1]
             annots[row[0]] = kept_annots
 
-    pops = od()
-    for l in pop_file:
-        tmp = l.rstrip().split('\t')
-        pops[tmp[0]] = tmp[1]
+    fam_dict = od()
+    for l in fam_file:
+        tmp = l.rstip().split()
+        fam_dict[tmp[1]] = tmp
 
     kept_samples = []
     sample_names = []
@@ -70,7 +70,7 @@ def make_ped_file(snp_list, annot_file, pop_file, gt_file, out_pre):
             if row[0] == "Probe Set ID" or row[0] == 'probeset_id':
                 for i, ind_tmp in enumerate(row[1:]):
                     ind = re.sub('(\.AxiomGT1\.chp Call Codes)|(\.CEL)', '', ind_tmp)
-                    if ind in pops:
+                    if ind in fam_dict:
                         kept_samples.append(i+1)
                         sample_names.append(ind)
             else:
@@ -78,7 +78,6 @@ def make_ped_file(snp_list, annot_file, pop_file, gt_file, out_pre):
                     gts[row[0]] = translate(annots[row[0]], [row[x] for x in kept_samples])
 
     with open('{}.ped'.format(out_pre), 'w') as ped_file, open('{}.map'.format(out_pre), 'w') as map_file:
-        ped_dict = dict(zip(sample_names, [[pops[x].replace(' ', '_'), x, '0', '0', '0', '0'] for x in sample_names]))
 
         for snp_id in annots:
             chrom = annots[snp_id][0]
@@ -87,10 +86,10 @@ def make_ped_file(snp_list, annot_file, pop_file, gt_file, out_pre):
 
             # transpose genotype matrix
             for ind, gt in zip(sample_names, gts[snp_id]):
-                ped_dict[ind].append(gt)
+                fam_dict[ind].append(gt)
 
         for ind in sample_names:
-            ped_file.write('\t'.join(ped_dict[ind]) + '\n')
+            ped_file.write('\t'.join(fam_dict[ind]) + '\n')
 
 
 def main():
@@ -110,17 +109,17 @@ def main():
                         type=lambda x: is_valid_file(parser, x),
                         dest='snp_list_fh', metavar="FILE")
 
-    parser.add_argument('-p', "--popfile",
-                        help="Populations file name.",
+    parser.add_argument('-f', "--famfile",
+                        help="PED family file, indicating the invidividuals desired for output.",
                         type=lambda x: is_valid_file(parser, x),
-                        dest='pop_fh', metavar="FILE")
+                        dest='fam_fh', metavar="FILE")
 
     parser.add_argument('-o', "--outname",
                         help="Prefix used for map and ped files. (default: out)",
                         type=str, dest='out_prefix', metavar="PREFIX", default='out')
     args = parser.parse_args()
 
-    make_ped_file(args.snp_list_fh, args.annot_fh, args.pop_fh, args.geno_fh, args.out_prefix)
+    make_ped_file(args.snp_list_fh, args.annot_fh, args.fam_fh, args.geno_fh, args.out_prefix)
 
 
 if __name__ == '__main__':
